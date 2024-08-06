c977de718319a08f86edc63a5de6ef08ecac0976
chee <yay@chee.party>
2024-08-06 18:25:15 +0100

    remove dist from committed files

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { useDocument } from './use-document.ts';
-export { useHandle } from './use-handle.ts';
-export { RepoContext, useRepo } from './use-repo.ts';
-//# sourceMappingURL=index.d.ts.map
--- dist/use-document.d.ts
+++
@@ -1,9 +0,0 @@
-import { AnyDocumentId, ChangeFn, Doc } from '@automerge/automerge-repo/slim';
-import { ChangeOptions } from '@automerge/automerge/slim/next';
-import { Resource } from 'solid-js';
-
-export declare function useDocument<T>(id: () => AnyDocumentId | undefined): [
-    Resource<Doc<T> | undefined>,
-    (changeFn: ChangeFn<T>, options?: ChangeOptions<T> | undefined) => void
-];
-//# sourceMappingURL=use-document.d.ts.map
